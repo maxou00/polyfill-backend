3440660e6150327fcebe43126247d1e77114da6c
themx05 <maximiliencomlan05@gmail.com>
2021-09-10 07:52:11 +0100

    added test to check is backend is all set

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { supabaseClient } from "./config";
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-    res.send("Hello, from Polyfill's backend");
-})
+app.get("/forms", async (req, res) => {
+    let result = await supabaseClient.from("forms").select("*");
+    res.json(result.data);
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Started server");
