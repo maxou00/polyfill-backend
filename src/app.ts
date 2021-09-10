a6af2d0aa8178fe3d46705d1d2a4b589fde7ad08
themx05 <maximiliencomlan05@gmail.com>
2021-09-10 07:34:07 +0100

    added test to check is backend is all set

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/hello", (req, res) => {
+    res.send("Hello, from Polyfill's backend");
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Started server");
 })
