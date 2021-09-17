ebe61c38846003cab9caa607deb09fedf2c6a372
themx05 <maximiliencomlan05@gmail.com>
2021-09-17 09:45:34 +0100

    added more logs

--- src/routes/middleware.ts
+++ src/routes/middleware.ts
@@ -5,8 +5,10 @@ export async function authenticateUser(req: Request, res: Response, next: NextFu
     let auth = req.headers['authorization'];
     if(auth) {
         let token = auth.replace("supabase ", "");
+        console.log(token);
         let rs = await supabaseClient.auth.api.getUser(token);
         if(rs.user) {
+            console.log("User is "+ rs.user.email);
             req.supaUser = rs.user;
         }
     }
